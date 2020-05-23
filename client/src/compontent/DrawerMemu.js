9cb4484fd7820f5c3b43ab8670627b30723bbc98
Che Yiping <uid14981@sansi.com>
2020-05-23 13:21:17 +0800

    feat:electron electron-builder

---
+++ client/src/compontent/DrawerMemu.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { makeStyles, createStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+
+const drawerWidth = 240;
+
+const drawerMemuStyle = makeStyles((theme) =>
+    createStyles({
+        drawer: {
+            width: drawerWidth,
+            flexShrink: 0,
+        },
+        drawerPaper: {
+            width: drawerWidth,
+            marginTop: 64
+        },
+        drawerHeader: {
+            display: 'flex',
+            alignItems: 'center',
+            padding: theme.spacing(0, 1),
+            // necessary for content to be below app bar
+            ...theme.mixins.toolbar,
+            justifyContent: 'flex-end',
+        },
+    }),
+);
+const generateDrawerMemu = (props) => {
+    const classes = drawerMemuStyle();
+    const { open } = props;
+
+    return (
+        <Drawer
+            className={classes.drawer}
+            variant="persistent"
+            anchor="left"
+            open={open}
+            classes={{
+                paper: classes.drawerPaper,
+            }}
+        >
+            <List>
+                {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+                    <ListItem button key={text}>
+                        <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                        <ListItemText primary={text} />
+                    </ListItem>
+                ))}
+            </List>
+            <Divider />
+        </Drawer>
+    );
+}
+
+export default generateDrawerMemu;
--- main.go
+++ main.go
@@ -8,7 +8,6 @@ import (
 func main() {
     router := gin.Default()
     //加载静态资源，一般是上传的资源，例如用户上传的图片
-    router.StaticFS("/", http.Dir("./static")) 
-
+    router.StaticFS("/", http.Dir("./static"))
     router.Run(":8080")
 }
